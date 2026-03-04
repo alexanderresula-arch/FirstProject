58557a24c3390a014a5421b77f18fa098a4d4a41
alexanderresula-arch <alexanderresula@gmail.com>
2026-03-04 11:52:39 +0800

    Second commit

--- obj/Debug/net10.0/FirstProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/FirstProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b046d5db691cef5c14cdbb8a465bed243e7e6a8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
