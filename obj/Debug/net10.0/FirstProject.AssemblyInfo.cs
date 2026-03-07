43694657c69b73212faeaa4222cf9114876bab54
alexanderresula-arch <alexanderresula@gmail.com>
2026-03-07 14:25:49 +0800

    Added a showcase of playtime and progress of characters in Forsaken

--- obj/Debug/net10.0/FirstProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/FirstProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d05caee7509d632a74d651dc1eedf923efe86865")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da383e2fd39e3e30d9920a71a157d17b61d3220f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
