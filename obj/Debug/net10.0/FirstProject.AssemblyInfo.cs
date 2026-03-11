e10f86698f2bdffc5654c84e98b0ef6e29498b5d
alexanderresula-arch <alexanderresula@gmail.com>
2026-03-11 12:27:37 +0800

    Forsaken BG not showing right

--- obj/Debug/net10.0/FirstProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/FirstProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43694657c69b73212faeaa4222cf9114876bab54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+461f7a9c181716418bb936992385f5337dadfdd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
